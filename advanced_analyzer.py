9aec2f1b97bd6c733455c3b99dad297338ec557e
Someshawtg <somesh.sharma@awtg.co.uk>
2025-02-28 17:22:55 +0000

    Update advanced_analyzer.py

--- advanced_analyzer.py
+++ advanced_analyzer.py
@@ -12,13 +12,6 @@ hide_elements = """
     .github-button {display: none;}
     </style>
 """
-st.markdown(hide_elements, unsafe_allow_html=True)
-
-hide_github_icon = “”"
-
-.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
-“”"
-st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 # Check if the image file exists
 if os.path.exists("awtg-new-logo.png"):
